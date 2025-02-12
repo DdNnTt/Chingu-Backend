0cef6fb5e68581ec1d54a59678c4e39c777d9535
jeongggggg <ahjeong921@gmail.com>
2025-02-13 01:19:05 +0900

    feat: 회원가입일, 마지막 로그인 날짜 자동 저장 추가

--- src/main/java/com/chingubackend/entity/User.java
+++ src/main/java/com/chingubackend/entity/User.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -23,7 +25,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,10 +46,10 @@ public class User {
     private String password;
 
     private String profilePictureUrl;
+
     private String bio;
 
-    @Column(updatable = false)
-    private LocalDateTime joinDate = LocalDateTime.now();
+    private LocalDateTime joinDate;
 
     private LocalDateTime lastLoginDate;
 
@@ -57,7 +58,31 @@ public class User {
     @Enumerated(EnumType.STRING)
     private SocialType socialType = SocialType.NONE;
 
+    @PrePersist
+    public void prePersist() {
+        if (joinDate == null) {
+            joinDate = LocalDateTime.now();  // 현재 시간으로 joinDate 설정
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        lastLoginDate = LocalDateTime.now();
+    }
+
     public void encodePassword(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
+    }
+
+    @Builder
+    public User(String userId, String name, String nickname, String email, String password, String profilePictureUrl, String bio, SocialType socialType) {
+        this.userId = userId;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.profilePictureUrl = profilePictureUrl;
+        this.bio = bio;
+        this.socialType = socialType;
     }
 }
