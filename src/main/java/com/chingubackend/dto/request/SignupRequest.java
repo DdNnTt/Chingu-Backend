1532cc22a22e7c21277e544a7137e4da16594a7c
jeongggggg <ahjeong921@gmail.com>
2025-02-13 00:21:08 +0900

    feat: SignupRequest 추가 - validation을 사용하여 빈 문자열, 공백 문자열 검사 및 이메일 형식 검사

---
+++ src/main/java/com/chingubackend/dto/request/SignupRequest.java
@@ -0,0 +1,25 @@
+package com.chingubackend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+public class SignupRequest {
+    @NotBlank(message = "아이디는 필수 입력 항목입니다.")
+    private String userId;
+
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    private String name;
+
+    @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
+    private String nickname;
+
+    @Email
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    private String password;
+
+}
