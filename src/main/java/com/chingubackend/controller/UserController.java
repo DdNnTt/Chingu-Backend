e801592c18a1769eac4431231b99dcb522d12afc
jeongggggg <ahjeong921@gmail.com>
2025-02-13 16:46:48 +0900

    Rename: JoinController -> UserController 로 파일명 변경

--- src/main/java/com/chingubackend/controller/UserController.java
+++ src/main/java/com/chingubackend/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class JoinController {
+public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
