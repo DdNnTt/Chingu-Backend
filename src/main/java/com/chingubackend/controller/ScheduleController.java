877b09a6ca1fdaa9131220c84ebb8ccf62ce3c1c
goldsonge <ty7968@gmail.com>
2025-03-05 22:31:10 +0900

    Feat : 개인 스케줄 조회 controller 코드 추가

--- src/main/java/com/chingubackend/controller/ScheduleController.java
+++ src/main/java/com/chingubackend/controller/ScheduleController.java
@@ -5,10 +5,9 @@ import com.chingubackend.entity.Schedule;
 import com.chingubackend.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/schedules")
@@ -20,5 +19,11 @@ public class ScheduleController {
     public ResponseEntity<Schedule> createSchedule(@RequestBody ScheduleRequest scheduleRequest) {
         Schedule savedSchedule = scheduleService.addSchedule(scheduleRequest);
         return ResponseEntity.ok(savedSchedule);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Schedule>> getSchedulesByUser(@PathVariable Long userId) {
+        List<Schedule> schedules = scheduleService.getSchedulesByUserId(userId);
+        return ResponseEntity.ok(schedules);
     }
 }
