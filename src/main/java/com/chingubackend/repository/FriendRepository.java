19cf6fe9da45832aa4165e79778c423a03d5f8e7
goldsonge <ty7968@gmail.com>
2025-03-21 16:02:13 +0900

    feat : 친구 요청 받은 목록 조회 repository 작성

--- src/main/java/com/chingubackend/repository/FriendRepository.java
+++ src/main/java/com/chingubackend/repository/FriendRepository.java
@@ -3,7 +3,10 @@ package com.chingubackend.repository;
 import com.chingubackend.entity.Friend;
 import com.chingubackend.model.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
@@ -11,4 +14,8 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     boolean existsByUserIdAndFriendIdAndRequestStatus(Long userId, Long friendId, RequestStatus status);
 
     Optional<Friend> findByUserIdAndFriendIdAndRequestStatus(Long friendId, Long userId, RequestStatus requestStatus);
+
+    @Query("SELECT f FROM Friend f WHERE f.friendId = :userId AND f.requestStatus = 'PENDING'")
+    List<Friend> findPendingRequestsForUser(@Param("userId") Long userId);
+
 }
