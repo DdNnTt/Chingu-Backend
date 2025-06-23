beb207348e07279c82de01a0c833d59419a4a2ee
jeongggggg <ahjeong921@gmail.com>
2025-06-23 19:31:18 +0900

    fix: 회원 탈퇴 시 추억 앨범 글 작성자 시스템 사용자로 이전 처리

--- src/main/java/com/chingubackend/repository/GroupMemoryRepository.java
+++ src/main/java/com/chingubackend/repository/GroupMemoryRepository.java
@@ -1,7 +1,14 @@
 package com.chingubackend.repository;
 
 import com.chingubackend.entity.GroupMemory;
+import com.chingubackend.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GroupMemoryRepository extends JpaRepository<GroupMemory, Long> {
+    @Modifying
+    @Query("UPDATE GroupMemory gm SET gm.user = :systemUser WHERE gm.user = :user")
+    void reassignMemoriesToSystem(@Param("user") User user, @Param("systemUser") User systemUser);
 }
